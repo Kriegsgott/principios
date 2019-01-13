3add1bb70a06b36c28b2d776284a343e901e8ffc
Kriegsgott <30702529+Kriegsgott@users.noreply.github.com>
2019-01-12 22:32:49 -0300

    display_fft update

--- receiver.py
+++ receiver.py
@@ -273,5 +273,7 @@ def display_fft(signal, fs):
     yf = fft(signal)
     xf = np.linspace(0.0, 1.0 / (2.0 * T), N // 2)
     plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]))
+    plt.title('FFT señal a transmitir')
+    plt.xlabel('Frecuencia HZ')
     plt.grid()
     plt.show()
